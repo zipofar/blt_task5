2a9e92c67ea6881d4bd9c48cbd010ceb0a52375d
Zipofar <zipofar@gmail.com>
2019-06-24 23:40:33 +0300

    wip vue

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Jopa',
+  },
+});
